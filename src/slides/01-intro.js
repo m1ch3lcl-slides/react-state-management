1d2bdca9a5ee15528710a184f747517a8c4f9108
Clément Michel <m1ch3lcl@gmail.com>
2018-03-14 12:04:03 +0100

    add gh-pages link

--- src/slides/01-intro.js
+++ src/slides/01-intro.js
@@ -10,5 +10,8 @@ export default (
     <Text textColor="tertiary" size={1} fit bold>
       State Management
     </Text>
+    <Text textColor="secondary" size={5} fit style={{marginTop: '50px'}}>
+      <a href="https://m1ch3lcl-slides.github.io/react-state-management">https://m1ch3lcl-slides.github.io/react-state-management</a>
+    </Text>
   </Slide>
 );
